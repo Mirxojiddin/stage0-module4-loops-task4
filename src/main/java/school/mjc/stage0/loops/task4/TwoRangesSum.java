d0a97486c7ba8f74c7a5c70b749c29b53617a955
Mirkhojiddin <71446322+Mirxojiddin@users.noreply.github.com>
2023-04-05 10:42:11 +0500

    Javob 2

--- src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
+++ src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
@@ -2,28 +2,23 @@ package school.mjc.stage0.loops.task4;
 
 public class TwoRangesSum {
     public void printSumOfTwoRanges(int numberToSkip, int lastInRow) {
-        int summ = 0;
+        int summ = 0,sum2=0, j=1;
         if (lastInRow<numberToSkip){
-           
-            for (int i=0; i<lastInRow; i++){
-                System.out.println("number to skip is bugger then the last");
-                summ+=i;
-            }
+            System.out.println("number to skip is bugger then the last");
         }
         else if (lastInRow<0)
             System.out.println("number to skip is bugger then the last");
             else {
-        
-                for (int i=0; i<lastInRow; i++)
-                    if (numberToSkip==i){
-                    System.out.println("skipped sum is number");
-                        continue;
-                    }
-                    else {
-                        System.out.println("counted sum is number");
-                        summ+=i;
-                    }
-            }
-            System.out.println(summ);
+                System.out.println("number to skip is bugger then the last");
+                for (int i = 0; i<=numberToSkip; i++){
+                    summ+=i;
+                    j=i;
+                }
+                    
+                for (int i=j; i<=numberToSkip; i++) 
+                    sum2+=i;
+                System.out.println("skipped sum is "+summ);
+                System.out.println("counted sum is "+sum2);
+                }
     }
 }
